08b697434253ea40b28d95ff0c3eda2b44f786f3
Антон <toshabespalov2006@gmail.com>
2022-10-25 15:39:10 +0300

    Update

--- src/main/java/project1/repositories/AddressRepository.java
+++ src/main/java/project1/repositories/AddressRepository.java
@@ -1,9 +1,7 @@
 package project1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import project1.domain.Address;
 
-@Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
 }
